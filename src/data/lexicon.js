6a0cc5f139bcd7873b036a36a0fda42e867bdf1b
phoet <phoetmail@googlemail.com>
2017-05-05 10:31:30 +0200

    add some pronouns nouns and abbreviations

--- src/data/lexicon.js
+++ src/data/lexicon.js
@@ -1,10 +1,10 @@
 'use strict';
 let lexicon = require('./misc');
 
-
 //add-in the arrays
 require('./maleNouns').forEach((str) => lexicon[str] = 'Mannlich');
 require('./femaleNouns').forEach((str) => lexicon[str] = 'Feminin');
+require('./neutralNouns').forEach((str) => lexicon[str] = 'Neutrum');
 require('./pronouns').forEach((str) => lexicon[str] = 'Pronomen');
 
 module.exports = lexicon;
