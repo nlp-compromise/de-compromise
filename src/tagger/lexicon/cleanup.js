f4c5242f2fb086385879cbd5c3b2326ee2ea3fbd
spencermountain <spencermountain@gmail.com>
2017-05-06 17:59:04 -0400

    clean lexicon

--- src/tagger/lexicon/cleanup.js
+++ src/tagger/lexicon/cleanup.js
@@ -11,3 +11,12 @@ Object.keys(data).forEach((k) => {
     }
   }
 });
+
+
+// let arr = data.verbs;
+// console.log(arr.length);
+// arr = arr.filter((s) => {
+//   return !data.infinitives.includes(s);
+// });
+// console.log(arr.length);
+// console.log(JSON.stringify(arr, null, 2));
