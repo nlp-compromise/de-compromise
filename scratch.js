8cde22632310018df5fa9330f9d793a9ed09b347
spencer kelly <spencerkelly86@gmail.com>
2023-04-17 15:48:52 -0400

    fix link to parsematch

--- scratch.js
+++ scratch.js
@@ -18,9 +18,24 @@ nlp.verbose('tagger')
 // let doc = nlp('kultureller').debug()
 
 let arr = [
-  'ich [muss] hinzufügen dass',
+  'skandalös',
+  'und der ganze Rest der skandalösen Sachen.',
+  'eine zweite Welle privater skandalöser Publikationen ausgelöst',
+  'ebenso Gerüchte über eine skandalöse, inzestuöse Verbindung mit seiner Halbschwester',
+  'Kraft',
+  'Fuss',
+  'anspruchsvoll',
+  'schöne',
+  // 'ich [muss] hinzufügen dass',
   // '[Mach] [dir] keine [Sorgen] ',//do not worry
 ]
-let doc = nlp(arr[0]).debug()
-doc.compute('root')
+let doc = nlp(arr[0]).debug()//.tag('Noun')
+// console.log(nlp.world().methods.two.transform.adjective.all)
+// console.log(doc.nouns().conjugate())
+// doc.compute('root')
+// console.log(doc.text('root'))
+// console.log(nlp.buildNet([{ match: '{skandalös}' }]))
+// console.log(nlp.parseMatch('{skandalös}')[0])
+
+
 
